6c4bdfb8e70df939cadc285df94674553b558512
ThamsanqaD <ThamsanqaD@MERA0564>
2025-01-29 14:45:31 +0200

    Add project files.

---
+++ ECommerce.SharedLibrary/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,73 @@
+﻿using eCommerce.SharedLibrary.Log;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "internal server error";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "too many request";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "not authorised";
+                    statusCode = (int)StatusCodes.Status401Unauthorized;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Access not allowed";
+                    message = "You are not allowed";
+                    statusCode = (int)StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+            }
+            catch (Exception ex) { 
+                LogException.LogExceptions(ex);
+
+                if (ex is TaskCanceledException || ex is TimeoutException) 
+                {
+                    title = "out of time";
+                    message = "Request timedout";
+                    statusCode = (int)StatusCodes.Status408RequestTimeout;
+                }
+
+                await ModifyHeader(context, title, message, statusCode);   
+            }
+        }
+
+        private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Title = title,
+                Detail = message,
+                Status = statusCode
+            }),CancellationToken.None);
+            return;
+        }
+    }
+}
---
+++ ECommerce.SharedLibrary/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Service not unavailable");
+                return;
+            }
+            else 
+                await next(context); 
+    }
+}
