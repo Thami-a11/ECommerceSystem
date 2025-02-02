17a23346f4c2da3aba7b67c2cf418a581a11dd91
Thami-a11 <thamsanqadyantyi4@gmail.com>
2025-02-02 22:07:49 +0200

    add file

---
+++ ECommerce.SharedLibrary/eCommerce.SharedLibrary/Logs/LogExceptions.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace eCommerce.SharedLibrary.Logs
+{
+    public static class LogExceptions
+    {
+        public static void LogException(Exception ex) 
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebugger(ex.Message);
+        }
+
+        public static void LogToFile(string message) => Log.Information(message);
+        public static void LogToConsole(string message) => Log.Warning(message);
+        public static void LogToDebugger(string message) => Log.Debug(message);
+    }
+}
